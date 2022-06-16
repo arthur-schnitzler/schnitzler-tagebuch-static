ce8386a506234173280842dcccb7ae07f48c800b
csae8092 <peter.andorfer@oeaw.ac.at>
2022-06-16 18:17:11 +0200

    tryout typesense for fulltext search

---
+++ html/js/ts_index.js
@@ -0,0 +1,87 @@
+const typesenseInstantsearchAdapter = new TypesenseInstantSearchAdapter({
+    server: {
+      apiKey: "xyz", // Be sure to use an API key that only allows search operations
+      nodes: [
+        {
+          host: "localhost",
+          port: "8108",
+          protocol: "http",
+        },
+      ],
+      cacheSearchResultsForSeconds: 2 * 60, // Cache search results from server. Defaults to 2 minutes. Set to 0 to disable caching.
+    },
+    // The following parameters are directly passed to Typesense's search API endpoint.
+    //  So you can pass any parameters supported by the search endpoint below.
+    //  query_by is required.
+    additionalSearchParameters: {
+      query_by: "full_text"
+    },
+  });
+
+
+const searchClient = typesenseInstantsearchAdapter.searchClient;
+const search = instantsearch({
+    indexName: 'stb',
+    searchClient,
+});
+
+search.addWidgets([
+    instantsearch.widgets.searchBox({
+        container: '#searchbox',
+    }),
+
+    instantsearch.widgets.hits({
+        container: '#hits',
+        templates: {
+            empty: 'No results',
+            item: `
+                <h4> {{ rec_id }}</h4>
+                <h5><a href="{{ rec_id }}">{{ title }}</a></h5>
+                <p>{{#helpers.snippet}}{ "attribute": "full_text" }{{/helpers.snippet}}</p>
+            `
+        }
+    }),
+
+    instantsearch.widgets.stats({
+        container: '#stats-container'
+    }),
+
+    instantsearch.widgets.refinementList({
+        container: '#refinement-list-places',
+        attribute: 'places',
+        searchable: true,
+    }),
+
+    instantsearch.widgets.refinementList({
+        container: '#refinement-list-persons',
+        attribute: 'persons',
+        searchable: true,
+    }),
+
+    instantsearch.widgets.refinementList({
+        container: '#refinement-list-works',
+        attribute: 'works',
+        searchable: true,
+    }),
+
+    instantsearch.widgets.pagination({
+        container: '#pagination',
+        padding: 2,
+    }),
+    instantsearch.widgets.clearRefinements({
+        container: '#clear-refinements',
+    })
+
+
+]);
+
+
+
+search.addWidgets([
+    instantsearch.widgets.configure({
+        attributesToSnippet: ['full_text'],
+    })
+]);
+
+
+search.start();
