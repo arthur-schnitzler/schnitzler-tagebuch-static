816fc8f1f9ab737eac12ab769f53080b11d55b8e
csae8092 <peter.andorfer@oeaw.ac.at>
2022-06-20 12:04:56 +0200

    populate central index as well

--- make_typesense_index.py
+++ make_typesense_index.py
@@ -5,22 +5,12 @@ import time
 import typesense
 
 from typesense.api_call import ObjectNotFound
+from acdh_cfts_pyutils import TYPESENSE_CLIENT as client
+from acdh_cfts_pyutils import CFTS_COLLECTION
 from acdh_tei_pyutils.tei import TeiReader
 from tqdm import tqdm
 
 files = glob.glob('./data/editions/*.xml')
-TYPESENSE_API_KEY = os.environ.get("TYPESENSE_API_KEY", "xyz")
-
-
-client = typesense.Client({
-  'nodes': [{
-    'host': os.environ.get('TYPESENSE_HOST','localhost'), # For Typesense Cloud use xxx.a1.typesense.net
-    'port': os.environ.get('TYPESENSE_PORT', '8108'),      # For Typesense Cloud use 443
-    'protocol': os.environ.get('TYPESENSE_PROTOCOL', 'http')   # For Typesense Cloud use https
-  }],
-  'api_key': TYPESENSE_API_KEY,
-  'connection_timeout_seconds': 120
-})
 
 try:
     client.collections['stb'].delete()
@@ -82,33 +72,50 @@ current_schema = {
 client.collections.create(current_schema)
 
 records = []
+cfts_records = []
 for x in tqdm(files, total=len(files)):
+    cfts_record = {
+        'project': 'sbt',
+    }
     record = {}
     doc = TeiReader(x)
     body = doc.any_xpath('.//tei:body')[0]
     record['rec_id'] = os.path.split(x)[-1]
+    cfts_record['rec_id'] = record['rec_id']
     record['id'] = os.path.split(x)[-1].replace('.xml', '')
+    cfts_record['id'] = record['id']
     record['title'] = " ".join(" ".join(doc.any_xpath('.//tei:title[@type="main"]//text()')).split())
+    cfts_record['title'] = record['title']
     date_str = doc.any_xpath('.//tei:title[@type="iso-date"]/text()')[0]
     record['year'] = int(date_str[:4])
+    cfts_record['id'] = record['id']
     try:
         ts = ciso8601.parse_datetime(date_str)
     except ValueError:
         ts = ciso8601.parse_datetime('1800-01-01')
 
     record['date'] = int(time.mktime(ts.timetuple()))
+    cfts_record['date'] = record['date']
     record['persons'] = [
         " ".join(" ".join(x.xpath('.//text()')).split()) for x in doc.any_xpath('.//tei:person/tei:persName')
     ]
+    cfts_record['persons'] = record['persons']
     record['places'] = [
          " ".join(" ".join(x.xpath('.//text()')).split()) for x in doc.any_xpath('.//tei:place[@xml:id]/tei:placeName')
     ]
+    cfts_record['places'] = record['places']
     record['works'] = [
          " ".join(" ".join(x.xpath('.//text()')).split()) for x in doc.any_xpath('.//tei:listBibl//tei:bibl[@xml:id]/tei:title')
     ]
+    cfts_record['works'] = record['works']
     record['full_text'] = " ".join(''.join(body.itertext()).split())
+    cfts_record['full_text'] = record['full_text']
     records.append(record)
+    cfts_records.append(cfts_record)
 
 make_index = client.collections['stb'].documents.import_(records)
 # print(make_index)
 print('done with indexing')
+
+make_index = CFTS_COLLECTION.documents.import_(cfts_records)
+print('done with central indexing')
