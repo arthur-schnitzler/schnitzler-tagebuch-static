b9fcad623c229aacc807441ade14cc47bd7a4bc3
Martin Anton Müller <martin.anton.mueller@oeaw.ac.at>
2022-11-07 14:01:59 +0100

    restored mentions

--- add_mentions.py
+++ add_mentions.py
@@ -7,7 +7,7 @@ from tqdm import tqdm
 
 
 files = glob.glob('./data/editions/*.xml')
-indices = glob.glob('./data/indices/listp*.xml')
+indices = glob.glob('./data/indices/list*.xml')
 
 d = defaultdict(set)
 for x in tqdm(sorted(files), total=len(files)):
