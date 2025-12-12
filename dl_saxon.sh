783eb7d810725fdab4c503685aa8a64322993337
martinantonmueller <martin.anton.mueller@oeaw.ac.at>
2025-12-12 23:13:11 +0100

    saxon lokal

--- dl_saxon.sh
+++ dl_saxon.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
 echo "downloading saxon"
+rm -f download
 wget https://sourceforge.net/projects/saxon/files/Saxon-HE/9.9/SaxonHE9-9-1-7J.zip/download && unzip download -d saxon && rm -rf download
